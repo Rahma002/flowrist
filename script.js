2f4de5d687fe4f15ca54f6696ba4bcabb3e2f97c
Rahmawati <rahma002wati@gmail.com>
2024-05-15 19:57:53 +0700

    final one

---
+++ script.js
@@ -0,0 +1,14 @@
+// Hamburger menu navbar - toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+///click body to remove nav hamburger
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
